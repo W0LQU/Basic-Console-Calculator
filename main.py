1f87bf1350a1c0a112c4106a1d1f5881ebdd0b51
W0LQU <wolqu@interia.pl>
2026-08-19 09:29:43 +0200

    update_2
    A basic calculator script using if/elif statements for addition, subtraction, multiplication, and division

--- main.py
+++ main.py
@@ -1,6 +1,28 @@
-#Dodawanie dwóch liczb
+#-------------Dodawanie dwóch liczb-------------
 
 #Liczba_1 = float(input("Podaj pierwszą liczbę: "))
 #Liczba_2 = float(input("Podaj drugą liczbę: "))
 #obliczenie = Liczba_1 + Liczba_2
 #print("Wynik dodawania to:", obliczenie)
+
+#-------------if-------------
+
+#Liczba_1 = float(input("Podaj pierwszą liczbę: "))
+#Liczba_2 = float(input("Podaj drugą liczbę: "))
+#x = input("Wybierz działanie (+, -, *, /): ")
+
+#if x == "+":
+#    obliczenie = Liczba_1 + Liczba_2
+#    print("Wynik dodawania to: ", obliczenie)
+#elif x == "-":
+#    obliczenie = Liczba_1 - Liczba_2
+#    print("Wynik odejmowania to: ", obliczenie)
+#elif x == "*":
+#    obliczenie = Liczba_1 * Liczba_2
+#    print("Wynik mnożenia to: ", obliczenie)
+#elif x == "/":
+#    if Liczba_2 == 0:
+#        print("nie dziel przez zero")
+#    else:
+#        obliczenie = Liczba_1 / Liczba_2
+#        print("Wynik dzielenia to: ", obliczenie)"

cde3ba930e88793ccfab8868f466aadaf4f11726
W0LQU <wolqu@interia.pl>
2026-08-19 08:58:45 +0200

    update_1
    A basic Python script that takes two numbers from user input, adds them together, and prints the result.

---
+++ main.py
@@ -0,0 +1,6 @@
+#Dodawanie dwóch liczb
+
+#Liczba_1 = float(input("Podaj pierwszą liczbę: "))
+#Liczba_2 = float(input("Podaj drugą liczbę: "))
+#obliczenie = Liczba_1 + Liczba_2
+#print("Wynik dodawania to:", obliczenie)
